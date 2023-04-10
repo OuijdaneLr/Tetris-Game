35f9d39411865286e27b5e474e74292a7f5508f9
OuijdaneLr <130411598+OuijdaneLr@users.noreply.github.com>
2023-04-11 01:20:48 +0200

    Update PuitsTest.java

--- test/fr/eseo/e3/poo/projet/blox/modele/PuitsTest.java
+++ test/fr/eseo/e3/poo/projet/blox/modele/PuitsTest.java
@@ -1,90 +1,3 @@
-/*package fr.eseo.e3.poo.projet.blox.modele;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
-import fr.eseo.e3.poo.projet.blox.modele.Coordonnees;
-import fr.eseo.e3.poo.projet.blox.modele.Couleur;
-import fr.eseo.e3.poo.projet.blox.modele.Puits;
-import fr.eseo.e3.poo.projet.blox.modele.pieces.IPiece;
-import fr.eseo.e3.poo.projet.blox.modele.pieces.OPiece;
-import org.junit.jupiter.api.BeforeEach;
-
-	 public class PuitsTest {
-
-	 	private Puits puits;
-
-	 	@BeforeEach
-	 	public void setUp() throws Exception {
-	 		this.puits = new Puits();
-	 	}
-
-	 	@Test
-	 	public void testConstructeur() {
-	 		assertEquals(Puits.LARGEUR_PAR_DEFAUT, this.puits.getLargeur());
-	 		assertEquals(Puits.PROFONDEUR_PAR_DEFAUT, this.puits.getProfondeur());
-
-	 		this.puits = new Puits(10, 20);
-	 		assertEquals(10, this.puits.getLargeur(),"Error");
-	 		assertEquals(20, this.puits.getProfondeur(),"Error");
-	 	}
-
-	 	@Test
-	 	public void testSetLargeur() {
-	 		assertThrows(IllegalArgumentException.class, () -> {
-	 			this.puits.setLargeur(4);
-	 		});
-	 		assertThrows(IllegalArgumentException.class, () -> {
-	 			this.puits.setLargeur(16);
-	 		});
-	 		this.puits.setLargeur(10);
-	 		assertEquals(10, this.puits.getLargeur());
-	 	}
-
-	 	@Test
-	 	public void testSetProfondeur() {
-	 		assertThrows(IllegalArgumentException.class, () -> {
-	 			this.puits.setProfondeur(14);
-	 		});
-	 		assertThrows(IllegalArgumentException.class, () -> {
-	 			this.puits.setProfondeur(26);
-	 		});
-	 		this.puits.setProfondeur(20);
-	 		assertEquals(20, this.puits.getProfondeur());
-	 	}
-
-	 	@Test
-	 	public void testGetSetPieceSuivante() {
-	 		OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
-	 		this.puits.setPieceSuivante(piece);
-
-	 		assertEquals(piece, this.puits.getPieceSuivante());
-
-	 		IPiece piece2 = new IPiece(new Coordonnees(0, 0), Couleur.JAUNE);
-	 		this.puits.setPieceSuivante(piece2);
-
-	 		assertEquals(piece, this.puits.getPieceActuelle());
-	 		assertEquals(piece2, this.puits.getPieceSuivante());
-	 	}
-
-	 	@Test
-	 	void testToString() {
-	 		String expected = "Puits : Dimension" + Puits.LARGEUR_PAR_DEFAUT + " * " + Puits.PROFONDEUR_PAR_DEFAUT
-	 				+ "\nPiece Actuelle : <aucune>\nPiece suivante : <aucune>\n";
-	 		assertEquals(expected, this.puits.toString());
-
-	 		OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
-	 		this.puits.setPieceSuivante(piece);
-
-	 		expected = "Puits : Dimension" + Puits.LARGEUR_PAR_DEFAUT + " * " + Puits.PROFONDEUR_PAR_DEFAUT
-	 				+ "\nPiece Actuelle : <aucune>\nPiece suivante : " + piece.toString() + "\n";
-	 		assertEquals(expected, this.puits.toString());
-	 	}
-	 }
-	
-
-*/
 
 package fr.eseo.e3.poo.projet.blox.modele;
 
@@ -159,17 +72,7 @@ public class PuitsTest {
 		puits.setProfondeur(20);
 		assertEquals(20, puits.getProfondeur());
 	}
-	/*
-	@Test(expected = IllegalArgumentException.class)
-	public void testSetProfondeurTooSmall() {
-		puits.setProfondeur(14);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void testSetProfondeurTooLarge() {
-		puits.setProfondeur(26);
-	}*/
-	
+
 	@Test
  	public void testGetSetPieceSuivante() {
  		OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
@@ -185,18 +88,5 @@ public class PuitsTest {
  	}
 	
 	
-//	@Test
-//	public void testToString() {
-//		
-//		String expected = "Puits : Dimension" + Puits.LARGEUR_PAR_DEFAUT + " x  " + Puits.PROFONDEUR_PAR_DEFAUT
-// 				+ "\nPiece Actuelle : <aucune>\nPiece suivante : <aucune>\n";
-// 		assertEquals(expected, puits.toString());
-//
-// 		OPiece piece = new OPiece(new Coordonnees(0, 0), Couleur.BLEU);
-// 		puits.setPieceSuivante(piece);
-//
-// 		expected = "Puits : Dimension" + Puits.LARGEUR_PAR_DEFAUT + " * " + Puits.PROFONDEUR_PAR_DEFAUT
-// 				+ "\nPiece Actuelle : <aucune>\nPiece suivante : " + piece.toString() + "\n";
-// 		assertEquals(expected, this.puits.toString());
-//	}
+
 }
